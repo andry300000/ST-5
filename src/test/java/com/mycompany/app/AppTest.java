09b94c7c06a178504533fafa64002d8280060e6a
andry300000 <andry.che.2005@gmail.com>
2026-06-07 22:12:45 +0300

    v2

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -3,7 +3,6 @@ package com.mycompany.app;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 
 public class AppTest {
@@ -11,73 +10,73 @@ public class AppTest {
     private static final double EPS = 0.0000001;
 
     @Test
-    public void constructorStoresArgument() {
+    public void test1() {
         Sqrt sqrt = new Sqrt(9.0);
         assertEquals(3.0, sqrt.calc(), EPS);
     }
 
     @Test
-    public void averageOfTwoAndFour() {
+    public void test2() {
         Sqrt sqrt = new Sqrt(1.0);
         assertEquals(3.0, sqrt.average(2.0, 4.0), EPS);
     }
 
     @Test
-    public void averageOfNegativeAndPositive() {
+    public void test3() {
         Sqrt sqrt = new Sqrt(1.0);
         assertEquals(0.0, sqrt.average(-2.0, 2.0), EPS);
     }
 
     @Test
-    public void goodReturnsTrueForExactGuess() {
+    public void test4() {
         Sqrt sqrt = new Sqrt(1.0);
         assertTrue(sqrt.good(3.0, 9.0));
     }
 
     @Test
-    public void goodReturnsFalseForInaccurateGuess() {
+    public void test5() {
         Sqrt sqrt = new Sqrt(1.0);
         assertFalse(sqrt.good(2.0, 9.0));
     }
 
     @Test
-    public void improveRefinesGuess() {
+    public void test6() {
         Sqrt sqrt = new Sqrt(1.0);
         assertEquals(2.5, sqrt.improve(1.0, 4.0), EPS);
     }
 
     @Test
-    public void iterReturnsGuessWhenAlreadyGood() {
+    public void test7() {
         Sqrt sqrt = new Sqrt(1.0);
         assertEquals(2.0, sqrt.iter(2.0, 4.0), EPS);
     }
 
     @Test
-    public void iterConvergesToSquareRoot() {
+    public void test8() {
         Sqrt sqrt = new Sqrt(1.0);
         assertEquals(2.0, sqrt.iter(1.0, 4.0), EPS);
     }
 
     @Test
-    public void calcReturnsOneForArgumentOne() {
+    public void test9() {
         Sqrt sqrt = new Sqrt(1.0);
         assertEquals(1.0, sqrt.calc(), EPS);
     }
 
     @Test
-    public void calcReturnsTwoForArgumentFour() {
+    public void test10() {
         Sqrt sqrt = new Sqrt(4.0);
         assertEquals(2.0, sqrt.calc(), EPS);
     }
 
     @Test
-    public void calcReturnsSqrtOfTwo() {
+    public void test11() {
         Sqrt sqrt = new Sqrt(2.0);
         assertEquals(Math.sqrt(2.0), sqrt.calc(), EPS);
     }
 
     @Test
-    public void calcReturnsThreeForArgumentNine() {
+    public void test12() {
         Sqrt sqrt = new Sqrt(9.0);
         assertEquals(3.0, sqrt.calc(), EPS);
     }
